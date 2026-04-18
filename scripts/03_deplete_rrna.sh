eb3fc27aae3f8293ddf597ce0360518ff4e9c8b4
Eibner-Gebhardt_ Madeline <mweibnergebhar@gulf.sciclone.wm.edu>
2026-04-18 16:36:55 -0400

    file path fixes

--- scripts/03_deplete_rrna.sh
+++ scripts/03_deplete_rrna.sh
@@ -10,8 +10,8 @@ OUT_DIR=./data/filtered
 for i in $(cat ./data/clean_metadata.tsv | cut -f5 | tail -n +2); 
 do CLEAN=${CLEAN_DIR}/${i/_1.fq.gz/_clean.fq.gz};
 WORK_DIR=${OUT_DIR}/${i/_1.fq.gz/_work};
-SAMPLE_NAME=${i/_1.fq.gz/_clean_filtered};
-DISCARD_NAME=${i/_1.fq.gz/_rrna};
+SAMPLE_NAME=${i/_1.fq.gz/_clean_filtered.fq.gz};
+DISCARD_NAME=${i/_1.fq.gz/_rrna.fq.gz};
 
 sortmerna --ref $RNA_DB --reads $CLEAN \
         --workdir $WORK_DIR \
